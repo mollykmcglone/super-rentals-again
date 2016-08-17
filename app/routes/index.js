c72096fbd809d3bcfc5cd5dadacc31a38811b549
Bo Zhao and Molly McGlone <bo+molly@>
2016-08-17 13:24:15 -0700

    updated rental and city models to implement a one-to-many relationship

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,12 +9,6 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    save3(params) {
-      var newRental = this.store.createRecord('rental', params);
-      newRental.save();
-      this.transitionTo('index');
-    },
-
     saveCity3(params) {
       var newCity = this.store.createRecord('city', params);
       newCity.save();
